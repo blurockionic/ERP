c4f435ce10036e2683fb0353751c3329a6b2ecb2
arun-upadhayay <arunupadhayay2000@gmail.com>
2024-03-01 10:48:25 +0530

    Create first page of erp make all the cards

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+
+import ButtonUsage from './components/ButtonUsage';
+import MainPage from './pages/mainErp/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <MainPage/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/BasicCard.jsx
@@ -0,0 +1,49 @@
+import { Card, CardActions, CardContent, Typography } from "@mui/material";
+import React from "react";
+import ButtonUsage from "./ButtonUsage";
+
+const BasicCard = ({ icon, title, description, handleOnOpen }) => {
+  return (
+    <div>
+      <Card
+        sx={{
+          backgroundColor: "#0000",
+          border: "2px solid #e5e7eb",
+          borderRadius: "none",
+          boxShadow: "none",
+        }}
+      >
+        <CardContent sx={{}}>
+          <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+            {" "}
+            <span>{icon}</span>
+            <Typography
+              sx={{ fontSize: 26, fontWeight: "700", marginLeft:'2px' }}
+              color="#581845"
+            >
+              {title}
+            </Typography>
+          </div>
+          <div style={{marginTop:"2px",width:"full", height:"1px", background:"black"}}></div>
+          <Typography variant="h5" component="div">
+            {/* be{bull}nev{bull}o{bull}lent */}
+          </Typography> 
+          kjndfknflkxv
+          <Typography sx={{}} color="text.secondary">
+            {description}
+          </Typography>
+          <Typography variant="body2">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <ButtonUsage onClick={handleOnOpen} children="Open" />
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
+
+export default BasicCard;
