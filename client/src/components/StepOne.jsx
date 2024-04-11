daf3af100d259ba4ddbc17bbed2da87b5e841035
arun-upadhayay <arunupadhayay10102000@gmail.com>
2024-04-11 10:43:36 +0530

    [change] make change in step one

--- client/src/components/StepOne.jsx
+++ client/src/components/StepOne.jsx
@@ -108,7 +108,7 @@ const StepOne = ({ nextStep }) => {
           <div></div>
         </div>
 
-        <div className="overflow-hidden overflow-x-hidden w-[80%] mx-auto bg-white border shadow-2xl h-[40rem]">
+        <div className="mt-8 overflow-hidden overflow-x-hidden w-[80%] mx-auto bg-white border shadow-2xl h-[30rem]">
           <div className="grid grid-cols-2 gap-8 m-4  mt-12 ">
             <div className="relative">
               <input
@@ -148,7 +148,9 @@ const StepOne = ({ nextStep }) => {
                 onChange={(e) => setCustomerPhoneNumber(e.target.value)}
               />
               <label class="flex w-full h-[40px] select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
-                Mobile Number <sub>*</sub>
+              <span className="relative">
+                  Mobile Number <sup className="text-red-800">*</sup>
+                </span>
               </label>
 
               {/* Remark for phone number */}
@@ -179,8 +181,8 @@ const StepOne = ({ nextStep }) => {
               <Datetime
                 inputProps={{
                   id: "dateTime",
-                  className:
-                    "peer w-full h-[40px] bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900",
+                  className: 
+                    "absolute w-full h-[40px] bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900",
                 }}
                 value={dateAndTime}
                 onChange={(movement) => setDateAndTime(movement)}
@@ -202,7 +204,7 @@ const StepOne = ({ nextStep }) => {
               </label>
             </div>
           </div>
-          <div className="flex justify-center mt-64">
+          <div className="flex justify-center mt-20">
             <button
               onClick={handleNext}
               disabled={isLoading} // Disable the button if loading
