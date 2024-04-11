2f861153e7cb572cf499e4c98aa7254ab31bd170
biruly-cyber <biruly2000@gmail.com>
2024-04-11 13:31:25 +0530

    [feature] add can send the invoice in whatsapp

--- whatsappBot/dbWatcher1.py
+++ whatsappBot/dbWatcher1.py
@@ -9,60 +9,69 @@ import time
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import os
+import fitz
+import datetime
 
+# convert pdf into png 
+async def convert_pdf_to_png(pdf_path, output_directory, image_name):
+    saved_files = ''
+    
+    # Get the current time
+    current_time = datetime.datetime.now()
 
-
-
+    # Format the time without milliseconds and replace colons with underscores
+    formatted_time = current_time.strftime("%H_%M_%S")
+    print(formatted_time)
+    
+    # Open the PDF
+    pdf_document = fitz.open(pdf_path)
+     
+     
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_directory, exist_ok=True)
+    
+    # Iterate through each page of the PDF
+    for page_number in range(len(pdf_document)):
+        # Get the page
+        page = pdf_document.load_page(page_number)
+        
+        # Render the page as a pixmap
+        pixmap = page.get_pixmap()
+        
+        # Save the pixmap as a PNG image
+        output_file = os.path.join(output_directory, f"IMG_{image_name}_{formatted_time}.png")
+        pixmap.save(output_file, "png")
+        
+        saved_files=output_file
+    
+    return saved_files
 #send convert information into pdf
 async def create_pdf(text, directory, filename):
-    # Ensure the directory exists, create it if it doesn't
-    os.makedirs(directory, exist_ok=True)
+    try:
+        # Ensure the directory exists, create it if it doesn't
+        os.makedirs(directory, exist_ok=True)
 
-    file_path = os.path.join(directory, filename)
+         # Sanitize the filename to replace spaces with underscores without removing the file extension
+        name, extension = os.path.splitext(filename)
+        filename = "".join(c if c.isalnum() or c in "-_." else "_" for c in name).rstrip("_") + extension
+        file_path = os.path.join(directory, filename)
+        
+        c = canvas.Canvas(file_path, pagesize=letter)
+        # Set up fonts
+        c.setFont("Helvetica", 12)
+        # Write the text to the PDF
+        textobject = c.beginText(100, 750)  # (x, y) starting position
+        for line in text.split('\n'):
+            textobject.textLine(line)
+        c.drawText(textobject)
+        # Close the PDF
+        c.save()
 
-    c = canvas.Canvas(file_path, pagesize=letter)
-    # Set up fonts
-    c.setFont("Helvetica", 12)
-    # Write the text to the PDF
-    textobject = c.beginText(100, 750)  # (x, y) starting position
-    for line in text.split('\n'):
-        textobject.textLine(line)
-    c.drawText(textobject)
-    # Close the PDF
-    c.save()
+        return file_path
+    except Exception as e:
+        print("Error creating PDF:", e)
+        return None
 
-    return file_path
-
-
-# Function to send PDF via WhatsApp
-async def send_pdf_whatsapp(pdf_file, recipients):
-    # Open WhatsApp Web
-    pw.open_web()
-
-    # Wait for WhatsApp Web to open
-    time.sleep(5)
-
-    # Iterate through each recipient
-    for recipient in recipients:
-        # Type recipient's name
-        pw.typewrite(recipient)
-        time.sleep(2)
-        pw.press('enter')
-        time.sleep(2)
-
-        # Attach file
-        pw.press('/')
-        time.sleep(2)
-        pw.typewrite(pdf_file)
-        time.sleep(2)
-        pw.press('enter')
-        time.sleep(2)
-
-        # Send message
-        pw.typewrite('Sending PDF file...')
-        time.sleep(2)
-        pw.press('enter')
-        time.sleep(2)
 
 # function for watch the db 
 async def watch_mongodb(db_name, collection_name):
@@ -72,13 +81,17 @@ async def watch_mongodb(db_name, collection_name):
         print("MongoDB connection successful.")
     except Exception as e:
         print("MongoDB connection failed:", e)
+        return
 
     db = client[db_name]
     collection = db[collection_name]
 
     cursor = collection.watch()
 
-    print(f"Watching Mongo db collection '{collection_name}' for changes....")
+    print(f"Watching MongoDB collection '{collection_name}' for changes....")
+    
+    # get customer order
+    
 
     for change in cursor:
         if change["operationType"] == "update":
@@ -87,60 +100,75 @@ async def watch_mongodb(db_name, collection_name):
             customer_address = latest_insert.get('customerAddress', 'N/A')
             customer_name = latest_insert.get('customerName', 'N/A')
             customer_time = latest_insert.get('dateAndTime', 'N/A')
-            isFinalOrderSubmited = latest_insert.get('isFinalOrderSubmited', 'N/A')
+            isFinalOrderSubmitted = latest_insert.get('isFinalOrderSubmitted', 'N/A')
             isBistarOrdered = latest_insert.get('isBistarOrdered', 'N/A')
             isTentOrdered = latest_insert.get('isTentOrdered', 'N/A')
             isLightOrdered = latest_insert.get('isLightOrdered', 'N/A')
             isCateringOrdered = latest_insert.get('isCateringOrdered', 'N/A')
             isDecorationOrdered = latest_insert.get('isDecorationOrdered', 'N/A')
 
-            print(isBistarOrdered, isTentOrdered, isLightOrdered, isCateringOrdered, isDecorationOrdered, isFinalOrderSubmited)
+            
+            # Assuming customer_time is a datetime object or string representation of a datetime
+            # If customer_time is already a string, you can skip this step
+            customer_time_str = str(customer_time)
+
+            # Parse the date string to a datetime object
+            datetime_obj = datetime.datetime.fromisoformat(customer_time_str)
+
+            # Extract only the date part
+            date_only = datetime_obj.date()
+
+            # Convert the date back to a string
+            date_only_string = date_only.isoformat()
+            
+            current_time = datetime.datetime.now()
+
+            # Format the time without milliseconds and replace colons with underscores
+            formatted_time = current_time.strftime("%H_%M_%S")
 
             # Check if final order is submitted
-            if isFinalOrderSubmited:  
-                # List of phone numbers
+            if isFinalOrderSubmitted:
+                # List of phone numbers 
+                 
+                 
                 recipients = ["+919506497032", f"+91{customer_phone_number}"]
-                
+
                 # Define the message to be sent
                 message = f"Thank You for choosing DG Caters Services!\nYour order has been successfully placed\nCustomer Phone No.-+91{customer_phone_number},\nCustomer Name:-{customer_name},\nCustomer Address:-{customer_address},\nDate:- {customer_time}\nOrder Details:- {'Bistar: YES' if isBistarOrdered else 'Bistar: NO'}, {'Tent: YES' if isTentOrdered else 'Tent: NO'}, {'Light: YES' if isLightOrdered else 'Light: NO'}, {'Catering: YES' if isCateringOrdered else 'Catering: NO'}, {'Decoration: YES' if isDecorationOrdered else 'Decoration: NO'}"
 
                 # File name for the PDF
-                file_name = f"{customer_name}_{customer_time}_order_details.pdf"
-                    
-                print(file_name)
-                
+                file_name = f"{customer_name}_{date_only_string}_{formatted_time}_order_details.pdf"
+                image_name = f"{customer_name}_{date_only_string}"
+
                 parent_directory = "C:/Users/blurock/Downloads/"
                 subdirectory = "DGCaters_Order"
-                
+
                 directory = os.path.join(parent_directory, subdirectory)
-                
+
                 pdf_file = await create_pdf(message, directory, file_name)
+
+                print(pdf_file)
                 
-                print(f"PDF saved to: {pdf_file}")
-    
-                if os.path.exists(pdf_file):
+                # Specify the PDF file path and the output directory
+               
+                output_directory = "C:/Users/blurock/Downloads/DGCaters_Order/image/"
+
+                # Convert the PDF to PNG images
+                saved_files = await convert_pdf_to_png(pdf_file, output_directory, image_name)
+                print("Saved files:", saved_files)
+                
+                if pdf_file:
                     print("File saved successfully.")
+                    # Loop through each recipient and send the message
+                    for recipient in recipients:
+                        # Wait for 2 seconds between each message to avoid rate limits
+                        time.sleep(2)
+                        pw.sendwhats_image(recipient, saved_files, message)
                 else:
-                    print("Error: File not saved.")
-                    
+                    print("Error: PDF file not created.")
+            else:
+                print("Final order not submitted.")
                 
-                # send send_whatsapp_message
-                # await send_pdf_whatsapp(pdf_file, recipients)
-                
-                # Convert PDF to image
-                # image_file = file_path[:-3] + "png"
-                # pywhatkit.from_pdf(file_path, image_file)
-                
-                #  # Wait for the image file to be generated
-                # while not os.path.exists(image_file):
-                #     await asyncio.sleep(1)      
-                                            
-                # Loop through each recipient and send the message
-                for recipient in recipients:
-                    # Wait for 2 seconds between each message to avoid rate limits
-                    time.sleep(2)
-                    pw.sendwhats_image(recipient, pdf_file, message)
-                    
 if __name__ == "__main__":
     database_name = "ERP_Solution"
     collection_name = "customers"
