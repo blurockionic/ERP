afe21e3efc05dd766cd4796631c66b4ed2442836
arun-upadhayay <arunupadhayay10102000@gmail.com>
2024-05-06 14:39:35 +0530

    chang: add the old save button in the order.js file for changing the status of order with new updateStatus funtion functionality

--- client/src/pages/orderManagement/customer/Customer.js
+++ client/src/pages/orderManagement/customer/Customer.js
@@ -2,16 +2,40 @@ import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { OrderDataContext } from "../../../context/OrderdataContext";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import config from "../../../config/config";
 
 const Customer = () => {
   const { allOrder } = useContext(OrderDataContext);
-  const [filteredCustomer, setFiteredCustomer] = useState([]);
+  const [allCustomer, setAllCustomer] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
+
+  // const [filteredCustomer, setFiteredCustomer] = useState([]);
+
   useEffect(() => {
-    setFiteredCustomer(
-      allOrder.filter((customer) => customer.status === "completed")
-    );
-  }, [allOrder]);
-  console.log("filter liye hue customer aa rahe ", filteredCustomer);
+    const fetchAllCustomer = async () => {
+      try {
+        const response = await axios.get(`${config.apiUrl}/customer/all`, {
+          withCredentials: true,
+        });
+
+        setIsLoading(false);
+        const { customers } = response.data;
+
+        setIsLoading(false);
+        setAllCustomer(customers);
+        
+      } catch (error) {
+        // Handle the error here, you can log it or show a message to the user
+        console.error("Error fetching orders:", error);
+      }
+    };
+
+    //invoke
+    fetchAllCustomer();
+  }, [isLoading]);
+  console.log(allCustomer);
+  // console.log("filter liye hue customer aa rahe ", filteredCustomer);
 
   const toggleToCustomerProfilePageHandler = (index) => {
     console.log("this is toggle ", index + 1);
@@ -19,82 +43,70 @@ const Customer = () => {
   return (
     <div>
       {/*  table and Add item div */}
-      <div className="h-[90%] overflow-y-scroll ">
+      <div className="h-[680px] ">
         {/* Add item div */}
-        <div className="">
-          <div className="pl-4">
-            <span className="text-3xl font-semibold ">Our Customer</span>
-          </div>
-          <div className=" bg-white border  p-3 rounded-md mt-4">
-            <div className="mt-2  table-container h-screen overflow-y-auto">
-              <table className="w-full text-center">
-                <thead className="sticky top-0 bg-white text-sm z-10">
-                  <tr className="text-gray-700 py-5">
-                    <th>SNo.</th>
 
-                    <th>Mobile Number</th>
-                    <th>Name </th>
-                    <th>Address</th>
-                    {/* <th>Date & Time </th> */}
-                    <th>Status</th>
-                    <th>More Details</th>
-                  </tr>
-                </thead>
-                <tbody className="text-sm font-normal overflow-y-auto mt-4 bg-white">
-                  {filteredCustomer.map((order, index) => (
-                    <tr
+        <div className="pl-4">
+          <span className="text-3xl font-semibold ">Our Customer</span>
+        </div>
+       
+          <div className="mt-2  table-container h-[90%] overflow-y-auto">
+            <table className="w-full text-center">
+              <thead className="sticky top-0 bg-white text-sm z-10">
+                <tr className="text-gray-700 py-5">
+                  <th>SNo.</th>
+
+                  <th>Mobile Number</th>
+                  <th>Name </th>
+                  <th>Address</th>
+                 
+                 
+                  <th>More Details</th>
+                </tr>
+              </thead>
+              <tbody className="text-sm font-normal overflow-y-auto mt-4 bg-white">
+                {allCustomer.map((order, index) => (
+                  <tr
                     key={index}
-                      className={`border-b text-center`}
-                      onClick={() => toggleToCustomerProfilePageHandler(index)}
-                      style={{ cursor: "pointer", height: "80px" }}
-                    >
-                      <td className="py-2 border-r-2 mx-auto font-bold">
-                        {index + 1}
-                      </td>
+                    className={`border-b text-center`}
+                    onClick={() => toggleToCustomerProfilePageHandler(index)}
+                    style={{ cursor: "pointer", height: "80px" }}
+                  >
+                    <td className="py-2 border-r-2 mx-auto font-bold">
+                      {index + 1}
+                    </td>
 
-                      <td className="py-2 text-center font-semibold">
-                        {order.customerPhoneNumber}
-                      </td>
-                      <td className="py-2 text-center capitalize font-bold">
-                        {order.customerName}
-                      </td>
-                      <td className="py-2 text-center">
-                        {order.customerAddress}
-                      </td>
-                      {/* <td className="py-2 text-center">{order.dateAndTime}</td> */}
-                      <td className="py-2 text-center relative">
-                        <span
-                          className={`${
-                            order.status === "active"
-                              ? "bg-blue-200 w-[5rem] text-center font-semibold py-1 px-3 rounded"
-                              : ""
-                          }`}
-                        >
-                          {order.status}
-                        </span>
-                      </td>
+                    <td className="py-2 text-center font-semibold">
+                      {order.customerPhoneNumber}
+                    </td>
+                    <td className="py-2 text-center capitalize font-bold">
+                      {order.customerName}
+                    </td>
+                    <td className="py-2 text-center">
+                      {order.customerAddress}
+                    </td>
+                   
 
-                      <td className="align-middle text-center relative">
-                        <Link
-                          to={{
-                            pathname: "customerProfileDetails",
-                            search: `?id=${order._id}`,
-                          }}
-                        >
-                          show
-                        </Link>
-                      </td>
-                      {/* <Link to={`customerProfileDetails/${order._id}`}>
+                    <td className="align-middle text-center relative">
+                      <Link
+                        to={{
+                          pathname: "customerProfileDetails",
+                          search: `?id=${order._id}`,
+                        }}
+                      >
+                        show
+                      </Link>
+                    </td>
+                    {/* <Link to={`customerProfileDetails/${order._id}`}>
                         <td>show</td>
                       </Link> */}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
-      </div>
+     
     </div>
   );
 };
