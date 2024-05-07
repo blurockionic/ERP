4b92b1851d15ba908711b732c3d26b2b716c49eb
biruly-cyber <biruly2000@gmail.com>
2024-05-07 12:52:18 +0530

    [fix] conflict in customer page

--- client/src/pages/orderManagement/customer/Customer.js
+++ client/src/pages/orderManagement/customer/Customer.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { useContext } from "react";
+import { OrderDataContext } from "../../../context/OrderdataContext";
 
 import { Link } from "react-router-dom";
 import axios from "axios";
 import config from "../../../config/config";
 
 const Customer = () => {
+  // const { allOrder } = useContext(OrderDataContext);
+  // const [filteredCustomer, setFiteredCustomer] = useState([]);
 
-  const { allOrder } = useContext(OrderDataContext);
-  const [filteredCustomer, setFiteredCustomer] = useState([]);
-
-
-  const [allCustomer, setAllCustomer] = useState([])
+  const [allCustomer, setAllCustomer] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   // const [filteredCustomer, setFiteredCustomer] = useState([]);
@@ -18,16 +18,15 @@ const Customer = () => {
   useEffect(() => {
     const fetchAllCustomer = async () => {
       try {
-        const response = await axios.get(`${config.apiUrl}/customer/all`, {
+        const response = await axios.get(`${config.apiUrl}/order/all`, {
           withCredentials: true,
         });
 
         setIsLoading(false);
-        const { customers } = response.data;
+        const { data } = response.data;
 
         setIsLoading(false);
-        setAllCustomer(customers);
-        
+        setAllCustomer(data);
       } catch (error) {
         // Handle the error here, you can log it or show a message to the user
         console.error("Error fetching orders:", error);
@@ -36,54 +35,40 @@ const Customer = () => {
 
     //invoke
     fetchAllCustomer();
-  }, [isLoading]);
-  console.log('all customre k data k andar kya kya fields present h ', allCustomer);
-
+  }, []);
+  console.log(
+    "all customre k data k andar kya kya fields present h ",
+    allCustomer
+  );
+  // console.log("filter liye hue customer aa rahe ", filteredCustomer);
 
   const toggleToCustomerProfilePageHandler = (index) => {
     console.log("this is toggle ", index + 1);
   };
   return (
-    <div>
-      {/*  table and Add item div */}
-      <div className="h-[680px] ">
-        {/* Add item div */}
+    <>
+      {" "}
+      <div>
+        {/* Table and Add item div */}
+        <div className="h-[680px]">
+          <div className="pl-4">
+            <span className="text-3xl font-semibold">Our Customers</span>
+          </div>
 
-
-                    <th>Mobile Number</th>
-                    <th>Name </th>
-                    <th>Address</th>
-                    {/* <th>Date & Time </th> */}
-                    {/* <th>Status</th> */}
-                    <th>More Details</th>
-                  </tr>
-                </thead>
-                <tbody className="text-sm font-normal overflow-y-auto mt-4 bg-white">
-                  {allOrder.map((order, index) => (
-                    <tr
-
-        <div className="pl-4">
-          <span className="text-3xl font-semibold ">Our Customer</span>
-        </div>
-       
-          <div className="mt-2  table-container h-[90%] overflow-y-auto">
+          <div className="mt-2 table-container h-[90%] overflow-y-auto">
             <table className="w-full text-center">
               <thead className="sticky top-0 bg-white text-sm z-10">
                 <tr className="text-gray-700 py-5">
                   <th>SNo.</th>
-
                   <th>Mobile Number</th>
-                  <th>Name </th>
+                  <th>Name</th>
                   <th>Address</th>
-                 
-                 
                   <th>More Details</th>
                 </tr>
               </thead>
               <tbody className="text-sm font-normal overflow-y-auto mt-4 bg-white">
                 {allCustomer.map((order, index) => (
                   <tr
-
                     key={index}
                     className={`border-b text-center`}
                     onClick={() => toggleToCustomerProfilePageHandler(index)}
@@ -92,30 +77,6 @@ const Customer = () => {
                     <td className="py-2 border-r-2 mx-auto font-bold">
                       {index + 1}
                     </td>
-
-
-                      <td className="py-2 text-center font-semibold">
-                        {order.customerPhoneNumber}
-                      </td>
-                      <td className="py-2 text-center capitalize font-bold">
-                        {order.customerName}
-                      </td>
-                      <td className="py-2 text-center">
-                        {order.customerAddress}
-                      </td>
-                      {/* <td className="py-2 text-center">{order.dateAndTime}</td> */}
-                      {/* <td className="py-2 text-center relative">
-                        <span
-                          className={`${
-                            order.status === "active"
-                              ? "bg-blue-200 w-[5rem] text-center font-semibold py-1 px-3 rounded"
-                              : ""
-                          }`}
-                        >
-                          {order.status}
-                        </span>
-                      </td> */}
-
                     <td className="py-2 text-center font-semibold">
                       {order.customerPhoneNumber}
                     </td>
@@ -125,8 +86,6 @@ const Customer = () => {
                     <td className="py-2 text-center">
                       {order.customerAddress}
                     </td>
-
-
                     <td className="align-middle text-center relative">
                       <Link
                         to={{
@@ -137,18 +96,15 @@ const Customer = () => {
                         show
                       </Link>
                     </td>
-                    {/* <Link to={`customerProfileDetails/${order._id}`}>
-                        <td>show</td>
-                      </Link> */}
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
         </div>
-     
-    </div>
+      </div>
+    </>
   );
 };
 
-export default Customer;
+export default Customer
