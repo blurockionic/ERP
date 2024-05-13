af06a9ae03d4379447e5f157a710df86863cd3e9
arun-upadhayay <arunupadhayay10102000@gmail.com>
2024-05-13 16:20:26 +0530

    customer profile details are created

--- client/src/pages/orderManagement/customer/CustomerProfilePage.js
+++ client/src/pages/orderManagement/customer/CustomerProfilePage.js
@@ -6,7 +6,7 @@ import { OrderDataContext } from "../../../context/OrderdataContext";
 
 const CustomerProfilePage = () => {
   // const { id } = useParams();
- 
+
   // const id = searchParams.get("id");
 
   const location = useLocation();
@@ -21,11 +21,7 @@ const CustomerProfilePage = () => {
   const [isEditing, setIsEditing] = useState(false);
   const { allOrder } = useContext(OrderDataContext);
 
-
-
-
   console.log(customerName, allOrder);
-
 
   console.log("filter kiya hua deta a raha h", details);
 
@@ -103,17 +99,6 @@ const CustomerProfilePage = () => {
           >
             Order History
           </span>
-          <div
-            className={`px-3 py-1.5 m-1 rounded-md font-semibold cursor-pointer ${
-              orderderCategoryActive
-                ? "bg-white border-b-2 border-black"
-                : "bg-transparent"
-            }`}
-            onClick={() => tabButtonhandler("orderCategory")}
-          >
-            {" "}
-            Order Category
-          </div>
         </div>
 
         {/* Profile details tab active */}
@@ -122,9 +107,9 @@ const CustomerProfilePage = () => {
             {/* add item div */}
 
             {/*  table and Add item div */}
-            <div className="flex flex-row ">
+            <div className=" grid grid-cols-1">
               {/* left side div  */}
-              <div className=" w-[40rem] flex border-r-2 ">
+              <div className="">
                 {/* Add item div */}
 
                 <div className=" py-4  w-full grid grid-cols-2 gap-x-16 gap-y-4 px-12">
@@ -133,21 +118,20 @@ const CustomerProfilePage = () => {
                   </div>
                   <div className="flex flex-col">
                     <span className="font-semibold py-2">First Name</span>
-                    <span className=" "> {customerAllOrder[0]?.customerName} </span>
+                    <span className=" ">
+                      {" "}
+                      {customerAllOrder[0]?.customerName}{" "}
+                    </span>
                   </div>
 
                   <div className="flex flex-col">
                     <span className="font-semibold py-2 ">Phone Number </span>
                     <span className=" text-sm ">
-                      <span>+91</span> {customerAllOrder[0]?.customerPhoneNumber}
+                      <span>+91</span>{" "}
+                      {customerAllOrder[0]?.customerPhoneNumber}
                     </span>
                   </div>
-                  {/* <div className="flex flex-col">
-                    <span className="font-semibold py-2 ">
-                      WhatsApp Number{" "}
-                    </span>
-                    <span className="text-sm ">{details[0].customerPhoneNumber }</span>
-                  </div> */}
+
                   <div className="flex flex-col">
                     <span className="font-semibold py-2 ">Email </span>
                     <span className="text-sm ">
@@ -162,31 +146,17 @@ const CustomerProfilePage = () => {
                       {customerAllOrder[0]?.customerAddress}
                     </span>
                   </div>
-                  {/* <div className="flex flex-col">
-                    <span className="font-semibold py-2 ">City </span>
-                    <span className="text-sm ">Ludhiana </span>
-                  </div>
-                  <div className="flex flex-col">
-                    <span className="font-semibold py-2 ">State</span>
-                    <span className="text-sm ">Pujab</span>
-                  </div> */}
                 </div>
               </div>
-              {/* right side div */}
-              <div className=" w-[40rem] flex">
-                {/* Add item div */}
-                <div className=" py-4  w-full grid grid-cols-1 gap-x-16 gap-y-4 px-12">
-                  <h1 className="font-bold text-2xl">Location</h1>
-                </div>
-              </div>
+            
             </div>
 
-            <button
+            {/* <button
               onClick={handleOnUpadateUserDetails}
               className="rounded py-2 px-6 text-center align-middle text-xs font-bold bg-white border shadow-md transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             >
               {isEditing ? "Save" : "Update"}
-            </button>
+            </button> */}
           </div>
         )}
         {/* costomer order History */}
@@ -195,21 +165,96 @@ const CustomerProfilePage = () => {
             {/* add item div */}
 
             <div>
-              {/* Table displaying customerAllOrder data */}
-              <table>
-                <thead>
-                  <tr>
-                    <th>Customer Phone Number</th>
+              <div
+                className="h-[5rem] w-[10rem] border p-2 flex flex-col rounded
+             "
+              >
+                <span className="text-lg font-semibold text-center ">
+                  Total order
+                </span>
+                <span className="font-bold text-2xl text-center text-green-600">
+                  {customerAllOrder.length}
+                </span>{" "}
+              </div>
+            </div>
+            <div className="mt-2 table-container h-[90%] overflow-y-auto">
+              <table className="w-full text-center">
+                <thead className="sticky top-0 bg-white text-sm z-10">
+                  <tr className="text-gray-700 py-5">
                     {/* Add more table headers as needed */}
+                    <th>S No.</th>
+                    <th>Order</th>
+                    <th>Date</th>
+                    <th>Status</th>
+                    <th>Order Category</th>
                   </tr>
                 </thead>
-                <tbody>
-                  {customerAllOrder.map((order, index) => (
-                    <tr key={index}>
-                      <td>{order.customerPhoneNumber}</td>
-                      {/* Add more table cells for other order data */}
-                    </tr>
-                  ))}
+                <tbody className="text-sm font-normal overflow-y-auto mt-4 bg-white">
+                  {customerAllOrder.map((order, index) => {
+                    // Parse the date string
+                    const date = new Date(order.dateAndTime);
+                    // Format the date to display only the date part
+                    const formattedDate = date.toLocaleDateString();
+
+                    return (
+                      <tr
+                        key={index}
+                        className={`border-b text-center`}
+                        // onClick={() => toggleToCustomerProfilePageHandler(index)}
+                        style={{ cursor: "pointer", height: "80px" }}
+                      >
+                        <td> {index + 1} </td>
+                        <td>{order.orderId}</td>
+                        <td>{formattedDate}</td>
+                        <td className="font-semibold">
+                          <div className="px-3 border-2 py-1 rounded-full border-gray-500 inline-block">
+                            <span
+                              className={`w-1.5 h-1.5 rounded-full inline-block mr-1 mb-0.5 ${
+                                order.orderStatus === "Completed"
+                                  ? "bg-gray-800"
+                                  : order.orderStatus === "In Progress"
+                                  ? "bg-blue-800"
+                                  : order.orderStatus === "Confirmed"
+                                  ? "bg-green-800"
+                                  : order.orderStatus ==="Not Confirmed"
+                                  ?"bg-violet-800"
+                                  : "bg-white" // Default color for other statuses
+                              }`}
+                            ></span>
+                            {order.orderStatus}
+                          </div>
+                        </td>
+                        {/* event order type  */}
+                        <td className="py-2  text-center ">
+                          {order.isLightOrdered && (
+                            <span className="bg-yellow-100 px-3 mx-1 cursor-pointer border py-1 rounded-full border-gray-500 inline-block">
+                              Light
+                            </span>
+                          )}
+                          {order.isTentOrdered && (
+                            <span className="bg-green-100 px-3 mx-1 cursor-pointer border py-1 rounded-full border-gray-500 inline-block">
+                              Tent
+                            </span>
+                          )}
+                          {order.isDecorationOrdered && (
+                            <span className="bg-slate-100 px-3 mx-1  cursor-pointer border py-1 rounded-full border-gray-500 inline-block">
+                              Decoration
+                            </span>
+                          )}
+                          {order.isBistarOrdered && (
+                            <span className="bg-blue-100 px-3 mx-1   cursor-pointer border py-1 rounded-full border-gray-500 inline-block">
+                              beding
+                            </span>
+                          )}
+                          {order.isCateringOrdered && (
+                            <span className="bg-red-100 px-3 mx-1  cursor-pointer border py-1 rounded-full border-gray-500 inline-block">
+                              Catering
+                            </span>
+                          )}
+                        </td>
+                      </tr>
+                    );
+                  })}
                 </tbody>
               </table>
 
