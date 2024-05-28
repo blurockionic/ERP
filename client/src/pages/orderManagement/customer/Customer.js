96ec02403813f3373750893c7defa04b78f5a0cf
biruly-cyber <biruly2000@gmail.com>
2024-05-28 12:53:54 +0530

    all page are responsive expect recipe module

--- client/src/pages/orderManagement/customer/Customer.js
+++ client/src/pages/orderManagement/customer/Customer.js
@@ -36,49 +36,50 @@ const Customer = () => {
 
   return (
     <>
-      {" "}
       <div>
         {/* Table and Add item div */}
         <div className="h-[680px]">
-          <div className="pl-4">
-            <span className="text-3xl font-semibold">Our Customers</span>
-          </div>
+          <div className=" table-container h-[90%] overflow-y-auto">
+            <table className="w-full">
+              <thead className="bg-white text-gray-900 sticky top-0 shadow-md uppercase">
+                <tr>
+                  <th className="py-2 px-4 text-left hidden sm:table-cell text-xs sm:text-sm">SNo.</th>
+                  <th className="py-2 px-4 text-left">Name</th>
+                  <th className="py-2 px-4 text-left">Mobile Number</th>
+                  {/* <th className="py-2 px-4 text-left">Address</th> */}
+                  <th className="py-2 px-4 text-left">More Details</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allCustomer.map((order, index) => (
+                  <tr
+                    key={index}
+                    className={
+                      index % 2 === 0 ? "bg-gray-100 h-16" : "bg-white h-16"
+                    }
+                  >
+                    <td className="py-2 px-4 font-semibold hidden sm:table-cell text-xs sm:text-sm">{index + 1}</td>
+                    <td className="py-2 px-4 capitalize font-semibold">
+                      {order.customerName}
+                    </td>
+                    <td className="py-2 px-4">{order.customerPhoneNumber}</td>
+                    {/* <td className="py-2 px-4">{order.customerAddress}</td> */}
 
-          <div className="mt-2 table-container h-[90%] overflow-y-auto">
-          <table className="w-full">
-  <thead className="bg-gray-200 text-gray-700">
-    <tr>
-      <th className="py-2 px-4 text-left">SNo.</th>
-      <th className="py-2 px-4 text-left">Mobile Number</th>
-      <th className="py-2 px-4 text-left">Name</th>
-      <th className="py-2 px-4 text-left">Address</th>
-      <th className="py-2 px-4 text-left">More Details</th>
-    </tr>
-  </thead>
-  <tbody>
-  {allCustomer.map((order, index) => (
-  <tr key={index} className={index % 2 === 0 ? "bg-gray-100 h-16" : "bg-white h-16"}>
-    <td className="py-2 px-4 font-semibold">{index + 1}</td>
-    <td className="py-2 px-4">{order.customerPhoneNumber}</td>
-    <td className="py-2 px-4 capitalize font-semibold">{order.customerName}</td>
-    <td className="py-2 px-4">{order.customerAddress}</td>
-    <td className="py-2 px-4">
-      <Link
-        to={{
-          pathname: "customerProfileDetails",
-          search: `?customerName=${order.customerName}`,
-        }}
-        className="text-blue-500 underline"
-      >
-        Show
-      </Link>
-    </td>
-  </tr>
-))}
-
-  </tbody>
-</table>
-
+                    <td className="py-2 px-4">
+                      <Link
+                        to={{
+                          pathname: "customerProfileDetails",
+                          search: `?customerName=${order.customerName}`,
+                        }}
+                        className="text-blue-500 underline"
+                      >
+                        Show
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
