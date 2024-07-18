fd96362285e0262d2d6b0eae1299311954f488c2
biruly-cyber <biruly2000@gmail.com>
2024-07-18 15:44:44 +0530

    logout and interate with based on company

--- backend/controller/AuthController.js
+++ backend/controller/AuthController.js
@@ -4,64 +4,6 @@ import crypto from "crypto";
 import mongoose from "mongoose";
 import User from "../model/auth_model.js";
 
-// //sign in
-// export const signIn = async (req, res, next) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const user = await User.findOne({ email });
-
-//     if (!user) return res.status(404).json({ message: "User not found" });
-
-//     const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-//     if (!isPasswordCorrect)
-//       return res.status(400).json({ message: "Invalid credentials" });
-
-//     const token = jwt.sign(
-//       { email: user.email, id: user._id },
-//       "dfahdirieirhfdajf",
-//       {
-//         expiresIn: "1h",
-//       }
-//     );
-
-//     res.status(200).json({ user: user, token });
-//   } catch (error) {
-//     next(errorHandler(500, "Something went wrong"));
-//   }
-// };
-
-// //sign up
-// export const signUp = async (req, res) => {
-//   const { username, email, password } = req.body;
-
-//   try {
-//     const existingUser = await User.findOne({ email });
-
-//     if (existingUser)
-//       return res.status(400).json({ message: "User already exists" });
-
-//     const hashedPassword = await bcrypt.hash(password, 12);
-
-//     const result = await User.create({
-//       username,
-//       email,
-//       password: hashedPassword,
-//     });
-
-//     const token = jwt.sign(
-//       { email: result.email, id: result._id },
-//       "dfahdirieirhfdajf",
-//       { expiresIn: "1h" }
-//     );
-
-//     res.status(201).json({ result, token });
-//   } catch (error) {
-//     res.status(500).json({ message: "Something went wrong" });
-//   }
-// };
-
 //google mail authentication
 export const google = async (req, res, next) => {
   const { name, email, photo } = req.body;
@@ -198,14 +140,12 @@ export const loginUser = async (req, res) => {
       }
     );
 
- 
-
     res
       .status(200)
-      .json({ success: true, token, user,  message: "Login successful." });
+      .json({ success: true, token, user, message: "Login successful." });
   } catch (error) {
     console.error("Error logging in user:", error);
-    res.status(500).json({ error: "Internal server error." });
+    return res.status(500).json({ error: "Internal server error." });
   }
 };
 
